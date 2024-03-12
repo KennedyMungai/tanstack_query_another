1ba2971fd790eb72257b75f436918921b3014c8a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 16:14:08 +0300

    Added some lifeCycle functions inside the useMutation hook

--- services/mutations.ts
+++ services/mutations.ts
@@ -4,5 +4,9 @@ import { useMutation } from '@tanstack/react-query'
 export const useCreateTodo = () =>
 	useMutation({
 		mutationKey: ['mutateTodos'],
-		mutationFn: (data: Todo) => createTodo(data)
+		mutationFn: (data: Todo) => createTodo(data),
+		onMutate: () => console.log('mutating'),
+		onError: (error, variables, context) => console.log(error),
+		onSuccess: (data, variables, context) => console.log(data),
+		onSettled: (data, error, variables, context) => console.log('Settled')
 	})
