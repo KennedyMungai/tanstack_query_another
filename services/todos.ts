bae995bf763bdfd5e77d12b111c361b932ca9d80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:20:45 +0300

    Created the todos service file

---
+++ services/todos.ts
@@ -0,0 +1,9 @@
+import { getTodoIds } from '@/lib/todos'
+import { useQuery } from '@tanstack/react-query'
+
+const useTodosIds = () => {
+	return useQuery({
+		queryKey: ['todos'],
+		queryFn: getTodoIds
+	})
+}
