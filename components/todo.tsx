ff39b7d7d6e5af54075876ea86f08198d24d0f5f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:54:47 +0300

    Got rid of the code not in use

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { useTodosIds } from '@/services/todos'
-import { useIsFetching } from '@tanstack/react-query'
 
 type Props = {}
 
 const Todo = (props: Props) => {
-	// const isFetching = useIsFetching()
-
 	const { error, data, isPending, isError, fetchStatus, status } =
 		useTodosIds()
 
@@ -16,18 +13,13 @@ const Todo = (props: Props) => {
 	if (isError) return <span>There is an error</span>
 
 	return (
-		<>
-			{/* <p>Query Function Status: {fetchStatus}</p> */}
-			{/* <p>Query Data Status: {status}</p> */}
-			{/* <p>Global isFetching: {isFetching}</p> */}
-			<div>
-				{data.map((todoId: number) => (
-					<p key={todoId} className='text-white'>
-						id: {todoId}
-					</p>
-				))}
-			</div>
-		</>
+		<div>
+			{data.map((todoId: number) => (
+				<p key={todoId} className='text-white'>
+					id: {todoId}
+				</p>
+			))}
+		</div>
 	)
 }
 
