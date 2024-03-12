d8ff28526ee2fdff8c9e698a599965e7eceec611
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:48:48 +0300

    Updated a function name

--- services/todos.ts
+++ services/todos.ts
@@ -1,9 +1,9 @@
-import { getTodoIds } from '@/lib/todos'
+import { getTodosIds } from '@/lib/todos'
 import { useQuery } from '@tanstack/react-query'
 
 export const useTodosIds = () => {
 	return useQuery({
 		queryKey: ['todos'],
-		queryFn: getTodoIds
+		queryFn: getTodosIds
 	})
 }
