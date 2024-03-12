a2cfa678c8af2751230aae5f65dabd5000fed449
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:24:12 +0300

    Created and rendered out the Todo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Todo from '@/components/todo'
+
 export default function Home() {
-	return <main></main>
+	return (
+		<main>
+			<Todo />
+		</main>
+	)
 }
---
+++ components/todo.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+type Props = {}
+
+const Todo = (props: Props) => {
+	return <div>Todo</div>
+}
+
+export default Todo
