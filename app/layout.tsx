847070002131019b1c655ac15a94692661b7e65a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 13:01:01 +0300

    Added the Tanstack Provider to the layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import TanstackProvider from '@/providers/tanstack-provider'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Tanstack Query Another",
-  description: "Yet another tanstack query application",
-};
+	title: 'Tanstack Query Another',
+	description: 'Yet another tanstack query application'
+}
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<TanstackProvider>
+				<body className={inter.className}>{children}</body>
+			</TanstackProvider>
+		</html>
+	)
 }
