a34367c898b99ea8ab16953f4a62dd6d7615e3ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:21:08 +0300

    Renamed the function getting the todos

--- lib/todos.ts
+++ lib/todos.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const getTodos = async () =>
+export const getTodoIds = async () =>
 	await axios
 		.get('http://localhost:3001/todos')
 		.then((res) => res.data.map((todo: Todo) => todo.id))
