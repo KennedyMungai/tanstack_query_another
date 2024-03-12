5e2ceb0fbf1f3b191fafa23531aa11043c1bcf30
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 13:04:51 +0300

    Made some few organizational changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<TanstackProvider>
-				<body className={inter.className}>{children}</body>
-			</TanstackProvider>
+			<body className={inter.className}>
+				<TanstackProvider>{children}</TanstackProvider>
+			</body>
 		</html>
 	)
 }
