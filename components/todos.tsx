486efa96bef415fcde8d25ad40d2fe628813daeb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 16:19:04 +0300

    Renamed a file

--- components/todos.tsx
+++ components/todos.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { useCreateTodo } from '@/services/mutations'
 import { useTodos, useTodosIds } from '@/services/todos'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 type Props = {}
 
-const Todo = (props: Props) => {
+const Todos = (props: Props) => {
 	const {
 		data: todoIdData,
 		isPending: isTodoIdPending,
@@ -13,12 +15,24 @@ const Todo = (props: Props) => {
 
 	const todosQueries = useTodos(todoIdData)
 
+	const createTodoMutation = useCreateTodo()
+
+	const {} = useForm<Todo>()
+
+	const handleCreateTodoSubmit: SubmitHandler<Todo> = (data: Todo) =>
+		createTodoMutation.mutate(data)
+
 	if (isTodoIdPending) return <span>...loading</span>
 
 	if (isTodoIdError) return <span>There is an error</span>
 
 	return (
 		<div>
+			<form
+				action=''
+				onSubmit={handleCreateTodoSubmit}
+				className=''
+			></form>
 			<ul>
 				{todosQueries.map(({ data }, index) => (
 					<li key={data?.id}>
@@ -34,4 +48,4 @@ const Todo = (props: Props) => {
 	)
 }
 
-export default Todo
+export default Todos
