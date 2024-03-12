5d88584f50d0a3ee92d35aa2872064096dc0e7fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 13:00:12 +0300

    Created the tanstack provider file

---
+++ providers/tanstack-provider.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+}
+
+const queryClient = new QueryClient()
+
+const TanstackProvider = ({ children }: Props) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	)
+}
+
+export default TanstackProvider
