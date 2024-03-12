2d4228d7d48f0920d968493196e4ddadb11ff252
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 15:48:30 +0300

    Added the getTodo function

--- lib/todos.ts
+++ lib/todos.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-export const getTodoIds = async () =>
+export const getTodosIds = async () =>
 	await axios
 		.get('http://localhost:3001/todos')
 		.then((res) => res.data.map((todo: Todo) => todo.id))
+
+export const getTodo = async (id: number) =>
+	await axios.get(`http://localhost:3001/todos/${id}`).then((res) => res.data)
