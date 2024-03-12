db580365911ed35f8228153279b48d6d24c0d411
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-12 13:13:21 +0300

    Defined the types

---
+++ types.d.ts
@@ -0,0 +1,16 @@
+interface Project {
+	id: number
+	name: string
+}
+
+interface Todo {
+	checked: boolean
+	title: string
+	description: string
+	id: number
+}
+
+interface Product {
+	id: number
+	name: string
+}
