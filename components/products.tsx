c04a48655a2adf450b606d90ad4210155dee4f72
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-13 13:03:48 +0300

    Infinite scrolling shenanigans

--- components/products.tsx
+++ components/products.tsx
@@ -1,9 +1,48 @@
 'use client'
 
+import { useProducts } from '@/services/todos'
+import { Fragment, useState } from 'react'
+
 type Props = {}
 
 const Products = (props: Props) => {
-	return <div>Products</div>
+	const productsQuery = useProducts()
+
+	const [selectedProduct, setSelectedProduct] = useState<number | null>(null)
+
+	return (
+		<div>
+			{productsQuery.data?.pages.map((group, index) => (
+				<Fragment key={index}>
+					{group.map((product) => (
+						<Fragment key={product.id}>
+							<button
+								key={product.id}
+								onClick={() => setSelectedProduct(product.id)}
+							>
+								{product.name}
+							</button>
+						</Fragment>
+					))}
+				</Fragment>
+			))}
+			<br />
+			<div>
+				<button
+					onClick={() => productsQuery.fetchNextPage()}
+					disabled={
+						productsQuery.isFetching || !productsQuery.hasNextPage
+					}
+				>
+					{productsQuery.isFetchingNextPage
+						? 'Loading More'
+						: productsQuery.hasNextPage
+						? 'Load More'
+						: ''}
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default Products
