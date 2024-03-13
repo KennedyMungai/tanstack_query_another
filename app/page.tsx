3136767b761ebbf28c072a30fa95ecd1f6fc1a7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-13 12:22:24 +0300

    Rendered outr the projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Projects from '@/components/Projects'
 import Todos from '@/components/todos'
 
 export default function Home() {
 	return (
 		<main>
+			<Projects />
 			<Todos />
 		</main>
 	)
